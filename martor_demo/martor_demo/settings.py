91c5dace992b9f12d7e3c526fdd3231d11d19059
Yurii Balashkevych <>
2023-03-22 15:28:39 +0200

    fix flake8 warnings

--- martor_demo/martor_demo/settings.py
+++ martor_demo/martor_demo/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 import os
-import tempfile
+# import tempfile
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
